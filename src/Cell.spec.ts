66ac5f34d4d7c6a11baad18180bd2dfa05cf0738
Tanausu <tanausu@acidtango.com>
2024-05-22 18:14:04 +0100

    define cell tests

---
+++ src/Cell.spec.ts
@@ -0,0 +1,15 @@
+import { describe, it, expect } from "vitest"
+import { myFunction } from "./main.js"
+
+describe("Cell", () => {
+  it.todo("can be created alive")
+  it.todo("can be created dead")
+  it.todo("dies if it has no neighbours")
+  it.todo("stays alives if it has 3 neighbours")
+  it.todo("stays alives if it has 2 neighbours")
+  it.todo("dies if it has 4 neighbours")
+  it.todo("dies if it has 8 neighbours")
+  it.todo("revives if it has 3 neighbours")
+  it.todo("stays dead if it has 2 neighbours")
+  it.todo("stays dead if it has 4 neighbours")
+})
